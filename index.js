ceb633060493049126553882448cb4b82442fcd5
kevin <live-mont@outlook.com>
2017-10-19 16:29:28 -0500

    este es mi primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const lodash= require('lodash')
+
+
+const obj={
+    nombre : 'kevin',
+    edad : '20',
+    email: 'oli_wars7@hotmail.com',
+}
+
+
+console.log(obj.email)
+/*
+function hola(){
+    return 'hola mundo -.-'
+}
+console.log("Hola node")
+
+console.log(hola());
+
+
+console.log(2+3);
+
+
+var are=[12,3,4,5,23]
+
+
+console.log(are);*/
